f8136441d8a1473b104d64112ab51439d3660901
Bigtruck2 <bigburrito363@gmail.com>
2025-05-14 11:46:48 -0400

    add idea files

--- Scantron.java
+++ Scantron.java
@@ -1,2 +1,12 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Scantron {
+    public static void main(String[] args) throws IOException {
+        File img = new File("images.jpg");
+        BufferedImage bufferedImage = ImageIO.read(img);
+        bufferedImage.getHeight();
+    }
 }
