3431cbaa6d96780b11c4fa7c48a29dd3e03abcb4
BT_1N3_24 <seymourb5@nycstudents.net>
2025-05-14 11:46:05 -0400

    First Day Done

--- ScoreDistribution.java
+++ ScoreDistribution.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
 public class ScoreDistribution {
+    public ArrayList<Score> Scores = new ArrayList<>();
+    public int numberOfScores = 0;
+
+    public ScoreDistribution(Score[] sco){
+        for (Score i : sco){
+            Scores.add(i);
+        }
+        numberOfScores = Scores.size();
+    }
+
+    public int classMean(){
+    }
 }
