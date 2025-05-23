22ff91a3bccb2904fb11a87f1963b6558bb66697
Bigtruck2 <bigburrito363@gmail.com>
2025-05-23 11:16:47 -0400

    clean scantron code

--- Scantron.java
+++ Scantron.java
@@ -1,10 +1,11 @@
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Optional;
 
 public class Scantron {
     static final int BLACK = -16777216;
@@ -15,21 +16,10 @@ public class Scantron {
     public static void main(String[] args) throws IOException {
         File img = new File("images.jpg");
         BufferedImage bufferedImage = ImageIO.read(img);
+        greyScale(bufferedImage);
+        ArrayList<Double> bubblex = new ArrayList<>();
         for (int i = 0; i < bufferedImage.getHeight(); i++) {
-            for (int j = 0; j < bufferedImage.getWidth(); j++) {
-                int rgb = bufferedImage.getRGB(j,i);
-                int r =(rgb>>16) & 0xFF;
-                int g =(rgb>>8) & 0xFF;
-                int b =(rgb) & 0xFF;
-                int grey = (Math.max(Math.max(r,b),g)+Math.min(Math.min(r,b),g))/2;
-                grey = (grey>150)?255:0;
-                int newPixel = (grey) | ((grey << 16)) | ((grey << 8));
-                bufferedImage.setRGB(j, i, newPixel);
-            }
-        }
-       // visited = new boolean[bufferedImage.getWidth()][bufferedImage.getHeight()];
-        for (int i = 0; i < bufferedImage.getHeight(); i++) {
-            for (int j = 0; j < bufferedImage.getWidth(); j++) {
+            for (int j = bufferedImage.getWidth()/2; j < bufferedImage.getWidth(); j++) {
                 if (bufferedImage.getRGB(j, i) == BLACK) {
                     ArrayList<Point>contour = traceContour(bufferedImage, new Point(j,i));
                     if (contour.size()>14){
@@ -45,19 +35,23 @@ public class Scantron {
                         int maxy = y.stream().max(Comparator.naturalOrder()).orElse(0);
                         int minx = x.stream().min(Comparator.naturalOrder()).orElse(0);
                         int miny = y.stream().min(Comparator.naturalOrder()).orElse(0);
+                        double centerX = (maxx + minx) / 2.0;
+                        double centerY = (maxy + miny) / 2.0;
                         int area = 0;
                         for (int k = minx; k < maxx; k++) {
                             for (int l = miny; l < maxy; l++) {
-                                if(bufferedImage.getRGB(k,l)==-16777216)area++;
-                                System.out.println(k);
-                                System.out.println(bufferedImage.getRGB(k,l));
+
+                                double rx = (maxx - minx) / 2.0;
+                                double ry = (maxy - miny) / 2.0;
+                                if(bufferedImage.getRGB(k,l)==-16777216&&isPointInEllipse(k - centerX, l - centerY, rx, ry))area++;
                             }
                         }
 
                         double circularity = 4 * Math.PI * area / (contour.size() * contour.size());
                         System.out.println(area);
                         System.out.println(circularity);
-                        if(circularity>.6) {
+                        if(circularity>.3&&area>8) {
+                            bubblex.add(centerX);
                             for (Point p : contour) {
                                 bufferedImage.setRGB(p.getX(), p.getY(), RED);
                             }
@@ -66,9 +60,54 @@ public class Scantron {
                 }
             }
         }
+        int centerBubbles = (int) ((bubblex.stream().max(Comparator.naturalOrder()).orElse(0.0)+bubblex.stream().min(Comparator.naturalOrder()).orElse(0.0))/2);
+        ArrayList<Double> ab = new ArrayList<>();
+        ArrayList<Double> cd = new ArrayList<>();
+
+        for (double x: bubblex){
+            if(centerBubbles<x){
+                ab.add(x);
+            }else {
+                cd.add(x);
+            }
+        }
+        int c = (int) ((ab.stream().max(Comparator.naturalOrder()).orElse(0.0)+ab.stream().min(Comparator.naturalOrder()).orElse(0.0))/2);
+        int a = (int) ((cd.stream().max(Comparator.naturalOrder()).orElse(0.0)+cd.stream().min(Comparator.naturalOrder()).orElse(0.0))/2);
+        int[] answers = new int[bubblex.size()];
+        for (int i = 0;i<bubblex.size();i++){
+            if(a>bubblex.get(i)){
+                answers[i]=0;
+            }else if(bubblex.get(i)>a&&bubblex.get(i)<centerBubbles) {
+                answers[i]=1;
+            }else if(bubblex.get(i)<c) {
+                answers[i]=2;
+            }else {
+                answers[i]=3;
+            }
+        }
+        System.out.println(Arrays.toString(answers));
+        for (int i = 0; i < bufferedImage.getHeight(); i++) {
+            bufferedImage.setRGB(a,i,GREEN);
+            bufferedImage.setRGB(c,i,GREEN);
+            bufferedImage.setRGB(centerBubbles,i,GREEN);
+        }
         ImageIO.write(bufferedImage, "jpg",img);
     }
-    public record Pair<U, V>(U perimeter, V meat) {}
+    public static void greyScale(BufferedImage bufferedImage){
+        for (int i = 0; i < bufferedImage.getHeight(); i++) {
+            for (int j = 0; j < bufferedImage.getWidth(); j++) {
+                int rgb = bufferedImage.getRGB(j,i);
+                int r =(rgb>>16) & 0xFF;
+                int g =(rgb>>8) & 0xFF;
+                int b =(rgb) & 0xFF;
+                int grey = (Math.max(Math.max(r,b),g)+Math.min(Math.min(r,b),g))/2;
+                grey = (grey>150)?255:0;
+                int newPixel = (grey) | ((grey << 16)) | ((grey << 8));
+                bufferedImage.setRGB(j, i, newPixel);
+            }
+        }
+    }
+    //inspired by https://github.com/biometrics/imagingbook/blob/master/src/contours/ContourTracer.java
     public static ArrayList<Point> traceContour(BufferedImage bufferedImage,  Point p){
         ArrayList<Point> perimeter = new ArrayList<>();
         perimeter.add(p);
@@ -94,5 +133,8 @@ public class Scantron {
             if (prevP.equals(p)) break;;
         } while (!p.equals(c));
         return perimeter;
+    }
+    public static boolean isPointInEllipse(double x, double y, double rx, double ry) {
+        return (x * x) / (rx * rx) + (y * y) / (ry * ry) <= 1.0;
     }
 }
